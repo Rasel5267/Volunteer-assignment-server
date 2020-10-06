3d1b819f94076183504c38fcf713212cdf2a008f
Rasel5267 <mahadiul09@gmail.com>
2020-10-06 13:51:24 +0600

    try again

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 
 
@@ -108,4 +108,4 @@ client.connect((err) => {
 });
 
 
-app.listen(PORT, console.log('server is starting at the ${PORT}'));
+app.listen(process.env.PORT || port);
